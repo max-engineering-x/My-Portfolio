c2d1cd7d038ae7f587f477f91f9fda3ef9760a70
Mohammed Alzahrani <moalsaadi18@gmail.com>
2025-01-25 16:09:51 +0300

    Finished with the expertise section/skills-section

---
+++ app/components/Expertises/Expertises.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import skills from "@/data/skills.json";
+import SectionContainer from "../Section/SectionContainer";
+import SectionHeader from "../Section/SectionHeader";
+
+import Expertise from "./Expertise";
+
+const Expertises = () => {
+  return (
+    <SectionContainer id="expertises">
+      <div className="section-contents mx-[22px] md:mx-[116px]">
+        <SectionHeader plainText="💻This is my" highlightText="Tech Stack" />
+        <div className="card w-full px-[33px] py-[27px] flex flex-wrap flex-col md:flex-row justify-center items-center gap-[19px] md:gap-[33px]">
+          {skills.map((skill, id) => (
+            <Expertise key={id} name={skill.name} icon={skill.icon} />
+          ))}
+        </div>
+      </div>
+      <>
+        <img
+          src="/tech_stack_grid_dark.svg"
+          alt="Background Grid Decoration"
+          width={569}
+          height={373}
+          className="hidden dark:md:block -z-10 absolute -left-[135px] -top-[39px]"
+        />
+           <img
+          src="/tech_stack_grid.svg"
+          alt="Background Grid Decoration"
+          width={569}
+          height={373}
+          className="hidden dark:hidden md:block -z-10 absolute -left-[125px] -top-[39px]"
+        />
+      </>
+    </SectionContainer>
+  );
+};
+
+export default Expertises;
